26103272d0fa4c1eac93b065c341f1f27dd381f5
Miguel <miguel.gf.130@gmail.com>
2023-06-25 16:05:06 -0600

    [Operador] Se crea su request para tener su validador propio

---
+++ app/Http/Requests/OperadorRequest.php
@@ -0,0 +1,58 @@
+<?php
+
+namespace App\Http\Requests;
+
+use App\Constants\ApiConstantes;
+use App\Helpers\ApiResponse;
+use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Http\Exceptions\HttpResponseException;
+use Illuminate\Contracts\Validation\Validator;
+
+class OperadorRequest extends FormRequest
+{
+	/**
+	 * Determine if the user is authorized to make this request.
+	 *
+	 * @return bool
+	 */
+	public function authorize()
+	{
+		return true;
+	}
+
+	/**
+	 * Get the validation rules that apply to the request.
+	 *
+	 * @return array<string, mixed>
+	 */
+	public function rules()
+	{
+		$rules = [
+			'nombre' => 'required',
+			'apellidos' => 'required',
+			'telefono' => 'nullable',
+		];
+
+		// if ($this->isMethod('PUT')) {
+		// 	$rules['campo1'] = 'required';
+		// }
+
+		return $rules;
+	}
+
+	public function messages()
+	{
+		return [
+			'nombre.required' => 'Nombre/s es obligatorio.',
+			'apellidos.required' => 'Los apellidos son obligatorios.',
+			'telefono.nullable' => '',
+			// Agrega aquí los mensajes de error personalizados para los demás campos
+		];
+	}
+
+	protected function failedValidation(Validator $validator)
+	{
+		$response = ApiResponse::error($validator->errors()->first(), ApiConstantes::ERROR_DATOS_INVALIDOS);
+		throw new HttpResponseException($response);
+	}
+}
