43cfb0727c5a2ea278760d4bbffe826a416e702b
Miguel <miguel.gf.130@gmail.com>
2023-06-24 22:55:32 -0600

    [Routes] Se cambia nomenclatura de rutas para versionamiento de API

---
+++ routes/api/api-v1.php
@@ -0,0 +1,8 @@
+<?php
+
+use App\Http\Controllers\OperadorController;
+use Illuminate\Support\Facades\Route;
+
+Route::prefix('operadores')->group(function () {
+    Route::get('/', [OperadorController::class, 'listar']);
+});
