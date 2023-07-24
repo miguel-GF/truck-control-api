21583a6e4b266fb5577684c07ee92e8b4651e8e4
Miguel <miguel.gf.130@gmail.com>
2023-07-23 21:52:23 -0600

    [Constantes] Se agrega archivo de constantes de ordenamiento

---
+++ app/Constants/OrdenarConstantes.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Constants;
+
+class OrdenarConstantes
+{
+  const NOMBRE_ASC = "nombre_asc";
+  const NOMBRE_DESC = "nombre_desc";
+  const CLAVE_ASC = "clave_asc";
+  const CLAVE_DESC = "clave_desc";
+  const REGISTRO_ASC = "fecha_registro_asc";
+  const REGISTRO_DESC = "fecha_registro_desc";
+}
